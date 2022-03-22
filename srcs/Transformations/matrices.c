5933c390ad778b059c9ccf00409e8561e1143756
Atom Collin <acollin@mi-p3.msk.21-school.ru>
2022-03-22 13:53:12 +0300

    Added specular light

--- srcs/Transformations/matrices.c
+++ srcs/Transformations/matrices.c
@@ -12,14 +12,18 @@
 
 #include "minirt.h"
 
-void	mult_vec_matrix(t_vec *src, t_vec *dst)
+void	mult_vec_matrix(t_vec *src, t_vec *dst, t_matrix *m)
 {
 	float	w;
 
-	dst->x = src->x * m[0][0] + src->y * m[1][0] + src->z * m[2][0] + m[3][0];
-	dst->y = src->x * m[0][1] + src->y * m[1][1] + src->z * m[2][1] + m[3][1];
-	dst->z = src->x * m[0][2] + src->y * m[1][2] + src->z * m[2][2] + m[3][2];
-	w = src->x * m[1][3] + src->y * m[1][3] + src->x * m[2][3] + m[3][3];
+	dst->x = src->x * m->mat[0][0] + src->y * m->mat[1][0]
+		+ src->z * m->mat[2][0] + m->mat[3][0];
+	dst->y = src->x * m->mat[0][1] + src->y * m->mat[1][1]
+		+ src->z * m->mat[2][1] + m->mat[3][1];
+	dst->z = src->x * m->mat[0][2] + src->y * m->mat[1][2]
+		+ src->z * m->mat[2][2] + m->mat[3][2];
+	w = src->x * m->mat[1][3] + src->y * m->mat[1][3]
+		+ src->x * m->mat[2][3] + m->mat[3][3];
 	if (w != 1 && w != 0)
 	{
 		dst->x /= w;
@@ -28,4 +32,55 @@ void	mult_vec_matrix(t_vec *src, t_vec *dst)
 	}
 }
 
-t_matrix	*cam_to_world()
+void	mult_dir_matrix(t_vec *src, t_vec *dst, t_matrix *m)
+{
+	float	a;
+	float	b;
+	float	c;
+
+	a = src->x * 1 + src->y * m->mat[1][0] + src->z * m->mat[2][0];
+	b = src->x * m->mat[0][1] + src->y * 1 + src->z * m->mat[2][1];
+	c = src->x * m->mat[0][2] + src->y * m->mat[1][2] + src->z * 1;
+	dst->x = a;
+	dst->y = b;
+	dst->z = c;
+}
+
+t_matrix	*look_at(t_vec *from, t_vec *to)
+{
+	t_vec		*forward;
+	t_vec		*right;
+	t_vec		*up;
+	t_vec		*tmp;
+	t_matrix	*cam_to_world;
+
+	tmp = vec_new(0, 1, 0);
+	forward = vec_subtraction(from, to);
+	vec_normalize(forward);
+	right = cross_product(tmp, forward);
+	up = cross_product(forward, right);
+	cam_to_world = new_matrix();
+	cam_to_world->mat[0][0] = right->x;
+	cam_to_world->mat[0][1] = right->y;
+	cam_to_world->mat[0][2] = right->z;
+	cam_to_world->mat[1][0] = up->x;
+	cam_to_world->mat[1][1] = up->y;
+	cam_to_world->mat[1][2] = up->z;
+	cam_to_world->mat[2][0] = forward->x;
+	cam_to_world->mat[2][1] = forward->y;
+	cam_to_world->mat[2][2] = forward->z;
+	cam_to_world->mat[3][0] = from->x;
+	cam_to_world->mat[3][1] = from->y;
+	cam_to_world->mat[3][2] = from->z;
+	return (cam_to_world);
+}
+
+t_matrix	*new_matrix()
+{
+	t_matrix	*m;
+
+	m = (t_matrix *)malloc(sizeof(t_matrix));
+	if (NULL == m)
+		return (NULL);
+	return (m);
+}
--- srcs/Utils/color.c
+++ srcs/Utils/color.c
@@ -12,10 +12,12 @@
 
 #include "minirt.h"
 
-int	get_sphere_color(t_vars *vars, t_inter *ret_inter, t_flist *figure_lst)
+int	get_sphere_color(t_vars *vars, t_inter *ret_inter, t_flist *figure_lst,
+						t_vec *ray_dir)
 {
 	t_sph	*tmp_sph;
 	float	cos_alpha;
+	float	specular;
 	t_vec	*light;
 	int		color_from_light;
 	t_inter	*shadow;
@@ -23,31 +25,42 @@ int	get_sphere_color(t_vars *vars, t_inter *ret_inter, t_flist *figure_lst)
 	tmp_sph = (t_sph *)ret_inter->figure;
 	shadow = NULL;
 	cos_alpha = 0;
+	specular = 0;
 	if (vars->light)
 	{
 		light = vec_subtraction(vars->light->d_point, ret_inter->point);
 		cos_alpha = (dot_product(ret_inter->norm, light)) / (vec_len(light));
 		vec_normalize(light);
 		shadow = intersect(light, figure_lst, ret_inter->point);
+		specular = specular_light(ret_inter->norm, light, ray_dir);
 		free(light);
 	}
 	if (shadow || cos_alpha < 0)
+	{
 		cos_alpha = 0;
+		specular = 0;
+	}
 	color_from_light = ft_color(
-			tmp_sph->red * (vars->amb->l_rat + cos_alpha),
-			tmp_sph->green * (vars->amb->l_rat + cos_alpha),
-			tmp_sph->blue * (vars->amb->l_rat + cos_alpha));
+			tmp_sph->red * (vars->amb->l_rat + cos_alpha *
+				vars->light->bright  + specular),
+			tmp_sph->green * (vars->amb->l_rat + cos_alpha *
+				vars->light->bright  + specular),
+			tmp_sph->blue * (vars->amb->l_rat + cos_alpha *
+				vars->light->bright  + specular));
 	return (color_from_light);
 }
 
-int	get_plane_color(t_vars *vars, t_inter *ret_inter, t_flist *figure_lst)
+int	get_plane_color(t_vars *vars, t_inter *ret_inter, t_flist *figure_lst,
+					   t_vec *ray_dir)
 {
 	t_plane	*tmp_plane;
 	float	cos_alpha;
 	t_vec	*light;
 	int		color_from_light;
 	t_inter	*shadow;
+	float	specular;
 
+	specular = 0;
 	tmp_plane = (t_plane *)ret_inter->figure;
 	shadow = NULL;
 	cos_alpha = 0;
@@ -58,42 +71,59 @@ int	get_plane_color(t_vars *vars, t_inter *ret_inter, t_flist *figure_lst)
 		vec_normalize(light);
 		vec_mult(light, -1);
 		shadow = intersect(light, figure_lst, ret_inter->point);
+		specular = specular_light(ret_inter->norm, light, ray_dir);
 		free(light);
 	}
 	if (shadow || cos_alpha < 0)
+	{
 		cos_alpha = 0;
+		specular = 0;
+	}
 	color_from_light = ft_color(
-			tmp_plane->red * (vars->amb->l_rat + cos_alpha),
-			tmp_plane->green * (vars->amb->l_rat + cos_alpha),
-			tmp_plane->blue * (vars->amb->l_rat + cos_alpha));
+			tmp_plane->red * (vars->amb->l_rat + cos_alpha *
+				vars->light->bright + specular),
+			tmp_plane->green * (vars->amb->l_rat + cos_alpha *
+				vars->light->bright + specular),
+			tmp_plane->blue * (vars->amb->l_rat + cos_alpha *
+				vars->light->bright + specular));
 	return (color_from_light);
 }
 
-int	get_cylinder_color(t_vars *vars, t_inter *ret_inter, t_flist *figure_lst)
+int	get_cylinder_color(t_vars *vars, t_inter *ret_inter, t_flist *figure_lst,
+						  t_vec *ray_dir)
 {
 	t_cyl	*tmp_cyl;
 	float	cos_alpha;
 	t_vec	*light;
 	int		color_from_light;
 	t_inter	*shadow;
+	float	specular;
 
+	specular = 0;
 	tmp_cyl = (t_cyl *)ret_inter->figure;
 	shadow = NULL;
 	cos_alpha = 0;
 	if (vars->light)
 	{
 		light = vec_subtraction(vars->light->d_point, ret_inter->point);
-		cos_alpha = (dot_product(ret_inter->norm, light)) / (vec_len(light));
+		cos_alpha = (dot_product(light, ret_inter->norm)) / (vec_len(light));
 		vec_normalize(light);
 		shadow = intersect(light, figure_lst, ret_inter->point);
+		specular = specular_light(ret_inter->norm, light, ray_dir);
 		free(light);
 	}
 	if (shadow || cos_alpha < 0)
+	{
 		cos_alpha = 0;
+		specular = 0;
+	}
 	color_from_light = ft_color(
-			tmp_cyl->red * (vars->amb->l_rat + cos_alpha),
-			tmp_cyl->green * (vars->amb->l_rat + cos_alpha),
-			tmp_cyl->blue * (vars->amb->l_rat + cos_alpha));
+			tmp_cyl->red * (vars->amb->l_rat + cos_alpha *
+				vars->light->bright + specular),
+			tmp_cyl->green * (vars->amb->l_rat + cos_alpha *
+				vars->light->bright + specular),
+			tmp_cyl->blue * (vars->amb->l_rat + cos_alpha *
+				vars->light->bright + specular));
 	return (color_from_light);
 }
 
@@ -108,19 +138,16 @@ int	ft_color(int red, int green, int blue)
 	return (red << 16 | green << 8 | blue);
 }
 
-int	gradient(int startcolor, int endcolor, int iter, int iter_max)
+float	specular_light(t_vec *N, t_vec *L, t_vec *V)
 {
-	double	increment[3];
-	int		new[3];
-	int		newcolor;
+	float	specular;
+	float	NL;
+	t_vec	*R;
 
-	increment[0] = ((endcolor >> 16) - (startcolor >> 16)) / (iter + 1);
-	increment[1] = ((endcolor >> 8) & 0xFF - (startcolor >> 8) & 0xFF)
-		/ (iter + 1);
-	increment[2] = ((endcolor & 0xFF) - (startcolor & 0xFF)) / (iter + 1);
-	new[0] = ((startcolor) >> 16) + fabs(iter_max * increment[0]);
-	new[1] = (((startcolor) >> 8) & 0xFF) + fabs(iter_max * increment[1]);
-	new[2] = ((startcolor) & 0xFF) + fabs(iter_max * increment[2]);
-	newcolor = (new[0] << 16) + (new[1] << 8) + new[2];
-	return (newcolor);
+	vec_mult(L, -1);
+	NL =  2 * dot_product(N, L);
+	vec_mult(N, NL);
+	R = vec_subtraction(N, L);
+	specular = pow(dot_product(V, R), 200);
+	return (specular);
 }
